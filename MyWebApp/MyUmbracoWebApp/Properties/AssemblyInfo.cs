4d0b97b2a8ac9e04452db4c93f5c9b6017eb4178
Victor Nielsen <van@netcompany.com>
2018-10-18 09:01:12 +0200

    added new project;

--- MyWebApp/MyUmbracoWebApp/Properties/AssemblyInfo.cs
+++ MyWebApp/MyUmbracoWebApp/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MyWebApp")]
+[assembly: AssemblyTitle("MyUmbracoWebApp")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MyWebApp")]
+[assembly: AssemblyProduct("MyUmbracoWebApp")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("0d756e86-1fc0-41ac-b96a-89486261fc9a")]
+[assembly: Guid("e273330e-5e44-4d57-ab16-48edb150975d")]
 
 // Version information for an assembly consists of the following four values:
 //
