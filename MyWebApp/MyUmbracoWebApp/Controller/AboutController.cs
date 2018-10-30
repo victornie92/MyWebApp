b6b8f6a44837d8faf7923c9af1bb8e40ed6566b4
Victor Nielsen <van@netcompany.com>
2018-10-30 08:35:28 +0100

    added models;

---
+++ MyWebApp/MyUmbracoWebApp/Controller/AboutController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+using System.Web.UI.WebControls;
+using Umbraco.Core;
+using Umbraco.Web.Mvc;
+
+namespace MyUmbracoWebApp.Controller
+{
+    public class AboutController : SurfaceController
+    {
+        public ActionResult About()
+        {
+            var test = "";
+
+            return PartialView("About");
+        }
+    }
+}
